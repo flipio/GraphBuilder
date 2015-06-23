c2a1bf1073be570deabbe8a8c19f7d6a45fb57f4
Dragan Bajcic <dragan@bajcic.com>
2015-06-24 00:12:06 +0200

    reformat ALL THE CODE

--- app/dyole/helpers/common.js
+++ app/dyole/helpers/common.js
@@ -2,53 +2,53 @@
  * Created by filip on 3.6.15..
  */
 'use strict';
-define(['lodash'], function (_) {
+define(['lodash'], function(_) {
 
 //@body
 
-var Common = {
-    objectPropExists: function (prop) {
-        return typeof prop !== 'undefined';
+  var Common = {
+    objectPropExists: function(prop) {
+      return typeof prop !== 'undefined';
     },
 
-    checkObjectKeys: function (obj) {
-        return this.objectPropExists(obj) && _.size(obj) > 0;
+    checkObjectKeys: function(obj) {
+      return this.objectPropExists(obj) && _.size(obj) > 0;
     },
 
-    getPropType: function (prop) {
-        var type = typeof prop,
-            result;
+    getPropType: function(prop) {
+      var type = typeof prop,
+        result;
 
-        result = type;
+      result = type;
 
-        if (type === 'object') {
-            if (_.isArray(prop)) {
-                result = 'array';
-            }
+      if (type === 'object') {
+        if (_.isArray(prop)) {
+          result = 'array';
+        }
+      }
+
+      return result;
+    },
+
+    setConstraints: function(instance, constraints) {
+
+      _.forEach(constraints, function(constraint, prop) {
+
+        if (Common.objectPropExists(constraint)) {
+          if (Common.getPropType(constraint) == 'object') {
+            _.forEach(constraint, function(value, key) {
+              instance[prop][key] = value;
+            });
+          } else {
+            instance[prop] = constraint;
+          }
         }
 
-        return result;
-    },
-
-    setConstraints: function (instance, constraints) {
-
-        _.forEach(constraints, function(constraint, prop) {
-
-            if (Common.objectPropExists(constraint)) {
-                if (Common.getPropType(constraint) == 'object') {
-                    _.forEach(constraint, function(value, key) {
-                        instance[prop][key] = value;
-                    });
-                } else {
-                    instance[prop] = constraint;
-                }
-            }
-
-        });
+      });
     }
-};
+  };
 
 //@body
-return Common;
+  return Common;
 
 });
