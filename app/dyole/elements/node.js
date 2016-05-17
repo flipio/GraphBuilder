bbf8f25edd1736d4368bbf17fb50d8f6f151a3e7
Filip Jelic <jelic.filip@gmail.com>
2016-05-17 16:08:06 +0200

    button position now gets svg group el

--- app/dyole/elements/node.js
+++ app/dyole/elements/node.js
@@ -817,7 +817,7 @@ define([
                     });
 
                     if (typeof this.buttons.info.position === 'function') {
-                        this.buttons.info.position.call(this, this.infoButton);
+                        this.buttons.info.position.call(this, this.infoButton.getEl());
                     }
 
                     this.removeNodeButton = this.canvas.button({
@@ -839,7 +839,7 @@ define([
                     });
 
                     if (typeof this.buttons.delete.position === 'function') {
-                        this.buttons.info.position.call(this, this.removeNodeButton);
+                        this.buttons.info.position.call(this, this.removeNodeButton.getEl());
                     }
 
                     _self.el.push(_self.infoButton.getEl()).push(_self.removeNodeButton.getEl());
